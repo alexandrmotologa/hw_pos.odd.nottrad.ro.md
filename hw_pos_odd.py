41791c144813da3c7e5d1c43e3d3ab686597dd74
alexandrmotologa <al.motologa@gmail.com>
2020-07-26 15:54:09 +0300

    cam asa

---
+++ hw_pos_odd.py
@@ -0,0 +1,6 @@
+# value = 10
+# #print("Positive") if value >= 0 else print("Negative")
+
+value = int(input("Enter a number: "))
+print("Positive") if value >= 0 else print("Negative")
+print("Positive and odd") if (value % 2) == 1 and value >= 0 else print("The rest")
